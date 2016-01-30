5abd69351825046d920ff3b2b5a6a3ff0647d174
Spencer Smitherman <spencer.smitherman@gmail.com>
2016-01-29 18:52:57 -0800

    Added basic route and config setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var canvasSdk = require('optimizely-canvas-sdk');
+var config = require('../config.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { name: 'Spencer' });
+	if (req.query.signed_request != undefined){
+		var userContext = canvasSdk.extractUserContext(config.app_secret, req.query.signed_request);
+		var project_id = userContext.context.environment.current_project;
+  		res.render('index', { name: 'Spencer' });
+ 	} else {
+ 		res.render('index', { name: 'Spencer' })
+ 	}
 });
 
 module.exports = router;
